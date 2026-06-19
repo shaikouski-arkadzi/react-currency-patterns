033a270e6551066237f54677e951531df284f8b5
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-06-19 22:36:07 +0300

    Add Adapter to Facade

--- src/service/currency/facade.ts
+++ src/service/currency/facade.ts
@@ -1,11 +1,17 @@
+import { CurrencyAdapter } from "./adapter";
 import { makeRequest } from "./currencyService";
 import { type CurrencyResponse } from "./types";
 
 export const CurrencyFacade = {
   getLatestRates: async (symbols = ["PKR", "GBP", "EUR", "USD"]) => {
     const symbolsString = symbols.join(",");
-    return makeRequest<CurrencyResponse>("/rates/latest", "GET", {
-      symbols: symbolsString,
-    });
+    const rawData = await makeRequest<CurrencyResponse>(
+      "/rates/latest",
+      "GET",
+      {
+        symbols: symbolsString,
+      },
+    );
+    return CurrencyAdapter.formatRates(rawData);
   },
 };
