e7998fd8d3921cc90759fd29c800a63b0243d9e9
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-06-21 10:19:42 +0300

    Add working with URL in route. Generate searchParams

--- apps/server/src/routes/currency.routes.ts
+++ apps/server/src/routes/currency.routes.ts
@@ -12,5 +12,19 @@ router.all(/.*/, async (req: Request, res: Response): Promise<void> => {
       res.status(500).json({ error: "API ключ не настроен на сервере" });
       return;
     }
+
+    const endpoint = req.path;
+
+    const targetUrl = new URL(`${TARGET_API_BASE}${endpoint}`);
+
+    targetUrl.searchParams.append("apikey", API_KEY);
+
+    for (const [key, value] of Object.entries(req.query)) {
+      if (typeof value === "string") {
+        targetUrl.searchParams.append(key, value);
+      } else if (Array.isArray(value)) {
+        targetUrl.searchParams.append(key, value.join(","));
+      }
+    }
   } catch {}
 });
