bee5a14518a67f69a0d0694591436b0acfc1b7c8
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2026-06-21 10:34:32 +0300

    Add CurrencyRouter to app(api gateway)

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,16 +2,19 @@ import express, { Application } from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+import { CurrencyRouter } from "./routes/currency.routes";
+
 export const createApp = (): Application => {
-	const app = express();
+  const app = express();
 
-	// Global middlewares
-	app.use(cors());
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
-	app.use(helmet());
+  // Global middlewares
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(helmet());
 
-	// API routes
+  // API routes
+  app.use("/api/currency", CurrencyRouter);
 
-	return app;
+  return app;
 };
